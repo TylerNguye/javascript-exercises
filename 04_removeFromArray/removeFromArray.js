85c88907def67540b2e909c94906f8ea4990df4f
Tyler Nguyen <nguyenhtyler@gmail.com>
2024-08-05 20:12:52 -0400

    Complete exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...removeElems) {
+    for (let i = 0; i < arr.length; i++) {
+        for (const arg of removeElems) {
+            if (arg === arr[i]) {
+                arr.splice(i, 1)
+                i--
+            }
+        }
+    }
+    return arr
 };
 
 // Do not edit below this line
