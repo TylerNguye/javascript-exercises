1f25da54f4a99e2abcb5999bef846003e5518e35
Tyler Nguyen <nguyenhtyler@gmail.com>
2024-08-05 20:04:11 -0400

    Finish exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(str, n) {
+    returnStr = ''
+    if (n < 0) {
+        return 'ERROR'
+    }
+    for (let i = 0; i < n; i++) {
+        returnStr += str
+    }
+    return returnStr
 };
 
 // Do not edit below this line
