1bb947391a5687216b1bc9c8cc9fec708e96cbf6
Tyler Nguyen <nguyenhtyler@gmail.com>
2024-08-05 20:37:32 -0400

    Finish exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let sum = 0
+	for (const arg of arr) {
+    sum += arg
+  }
+  return sum
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  product = 1
+  for (const arg of arr) {
+    product *= arg
+  }
+  return product
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  return a**b
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let factorial = 1
+	for (let i = a; i > 0; i--) {
+    factorial *= i
+  }
+  return factorial
 };
 
 // Do not edit below this line
