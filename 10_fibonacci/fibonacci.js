abb593f07c03ea9edeb4f183ef66cb5a04d8cab7
Tyler Nguyen <nguyenhtyler@gmail.com>
2024-08-06 23:47:31 -0400

    Finish exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(position) {
+    position = Number(position)
+    if (position < 0) {
+        return "OOPS"
+    }
+    if (position === 0) {
+        return 0
+    }
+    sequence = [1, 1]
+    for (let i = 2; i < position; i++) {
+        sequence.push(sequence[i-1] + sequence[i-2])
+    }
+    return sequence[sequence.length - 1]
 };
 
 // Do not edit below this line
