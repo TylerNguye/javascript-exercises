aa917b6161d9c5faba5d0f94ad9a5e2fa45133ce
Tyler Nguyen <nguyenhtyler@gmail.com>
2024-08-07 00:08:56 -0400

    Finish exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,9 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return people.sort((a, b) => {
+        const age1 = ("yearOfDeath" in a ? a.yearOfDeath : new Date().getFullYear()) - a.yearOfBirth
+        const age2 = ("yearOfDeath" in b ? b.yearOfDeath : new Date().getFullYear()) - b.yearOfBirth
+        return age1 > age2 ? -1 : 1
+    })[0]
 };
-
 // Do not edit below this line
 module.exports = findTheOldest;
