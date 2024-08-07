7c71e2738e2b6d97cfd70c6deae2d79ba81170b5
Tyler Nguyen <nguyenhtyler@gmail.com>
2024-08-06 23:40:43 -0400

    Finish exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
-
+const palindromes = function (phrase) {
+    const str = phrase.replace(/[^a-zA-Z0-9]/g, "").toLowerCase()
+    reverseStr = ''
+    for (let i = str.length - 1; i >= 0; i--) {
+        reverseStr += str.charAt(i)
+    }
+    for (let i = 0; i < str.length - 1; i++) {
+        if (str.charAt(i) !== reverseStr.charAt(i)) {
+            return false
+        }
+    }
+    return true
 };
 
 // Do not edit below this line
